3c9ef70f9d11076d69464e5f4c9c34efd13cadaf
Leslie <leslie.gonzalez1799@gmail.com>
2022-10-28 13:18:41 -0500

    first changes

---
+++ ejercicio1/assets/js/app.js
@@ -0,0 +1,28 @@
+console.log("Hola");
+
+
+const baseEndpoint = 'https://api.github.com';
+const usersEndpoint = `${baseEndpoint}/users`;
+const $n = document.querySelector('name');
+const $b = document.querySelector('#blog');
+const $l = document.querySelector('.location');
+
+//Se gregó async a la función
+async function displayUser(username) {
+  $n.textContent = 'cargando...';
+  const response = await fetch(`${usersEndpoint}/${username}`);
+  console.log(data);
+  $n.textContent = '${data.name}';
+  $b.textContent = '${data.blog}';
+  $l.textContent = '${data.location}';
+}
+
+function handleError(err) {
+  let error = err;
+  console.log('OH NO!');
+  console.log(err);
+  //Se agregó $ a  n.textContent. Además de ; al final de la instrucción
+  $n.textContent = `Algo salió mal: ${error}`;
+}
+
+displayUser('stolinski').catch(handleError);
