54c931399707d116611f1b21a21b577d5e158bc1
Leslie <leslie.gonzalez1799@gmail.com>
2022-10-28 23:00:53 -0500

    cambios en js

--- ejercicio1/assets/js/app.js
+++ ejercicio1/assets/js/app.js
@@ -4,25 +4,23 @@ console.log("Hola");
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
 const $n = document.querySelector('.name');//Se agregó . porque es una clase
-const $b = document.querySelector('.blog');//Se cambió el # por . ya que es una clase en 
+const $b = document.querySelector('.blog');//Se cambió el # por . ya que es una clase
 const $l = document.querySelector('.location');
 
 //Se gregó async a la función
 async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
+  //console.log(data);
   $n.textContent = '${data.name}';
   $b.textContent = '${data.blog}';
   $l.textContent = '${data.location}';
 }
 
 function handleError(err) {
-  let error = err;
   console.log('OH NO!');
   console.log(err);
-  //Se agregó $ a  n.textContent. Además de ; al final de la instrucción
-  $n.textContent = `Algo salió mal: ${error}`;
+  $n.textContent = `Algo salió mal: ${err}`; //Se agregó $ a  n.textContent. Además de ; al final de la instrucción
 }
 
 displayUser('stolinski').catch(handleError);
